be4e103299a1824b36f23c64336049b23e7c1329
Angelito Diwata <diwataangelito@gmail.com>
2022-09-15 23:46:05 +0800

    Added Pagination + Design revamp and dockerfile

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const knex = require('knex')({
     connection: {
         host: 'localhost',
         port: 3306,
-        user: 'root1',
+        user: 'root',
         password: '',
         database: 'project_master'
     }
